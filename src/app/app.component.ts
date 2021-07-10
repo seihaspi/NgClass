5b6b247c6eb1eeb0c9656898d4276e8f9c48e2ae
seihaspi <younseyha988@gmail.com>
2021-07-11 02:31:19 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngClass';
+  primaryColor = "primary-color";
+  backGroundColor ="container-bg";
+
+  ChangeTheme(){
+    if(this.primaryColor == "primary-color"){
+      this.primaryColor = "secondary-color";
+    }else{
+      this.primaryColor = "primary-color";
+    }
+  }
+  changeBackround(){
+    if(this.backGroundColor == "container-bg"){
+      this.backGroundColor = "container-bg1";
+    }else{
+      this.backGroundColor = "container-bg";
+    }
+  }
 }
